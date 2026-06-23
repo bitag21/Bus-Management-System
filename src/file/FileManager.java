126b625570061e21787bcb0b78471da184271964
Tsi6 <tsionyazachew4@gmail.com>
2026-06-23 19:27:35 +0300

    Add FileManager and Main classes

---
+++ src/file/FileManager.java
@@ -0,0 +1,81 @@
+package file;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileManager {
+    
+    // save objects using serialization
+    public void saveObjects(String fileName, ArrayList<?> list) {
+
+        try(ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(fileName))){
+
+            output.writeObject(list);
+            System.out.println("Data saved successfully to " + fileName);
+
+         } catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    // load objects using Serialization
+    public ArrayList<?> loadObjects(String fileName){
+        
+        try(ObjectInputStream input = new ObjectInputStream(new FileInputStream(fileName))){
+
+             return(ArrayList<?>) input.readObject();
+
+         } catch(IOException | ClassNotFoundException e){
+
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return new ArrayList<>();
+    }
+
+    //write text to a file(BufferedWriter)
+    public void writeTextFile(String fileName, String text) {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+
+            writer.write(text);
+            System.out.println("Text written successfully.");
+
+        } catch(IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    //read text from a file(BufferedReader)
+    public void readTextFile(String fileName) {
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch(IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    //Create a new file
+    public void createFile(String fileName) {
+
+        try {
+            File file = new File(fileName);
+
+            if (file.createNewFile()) {
+                System.out.println("File created successfully. ");
+
+            } else {
+                System.out.println("File already exists.");
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,230 @@
+package main;
+
+import model.*;
+import service.*;
+import file.FileManager;
+import database.DBConnection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static ArrayList<Passenger> passengers = new ArrayList<>();
+    static ArrayList<Driver> drivers = new ArrayList<>();
+
+    static BusService bS = new BusService();
+    static TicketService tS = new TicketService();
+
+    static FileManager fM = new FileManager();
+    static DBConnection db = new DBConnection();
+
+    public static void displayMenu(){
+        
+            System.out.println("\n============================");
+            System.out.println("    BUS MANAGEMENT SYSTEM    ");
+            System.out.println("============================");
+            System.out.println("1. Add Driver ");
+            System.out.println("2. Add Bus ");
+            System.out.println("3. Add Passenger ");
+            System.out.println("4. Book Ticket ");
+            System.out.println("5. Display All Buses ");
+            System.out.println("6. Display All Tickets ");
+            System.out.println("7. Save Data to File ");
+            System.out.println("8. Load Data from File ");
+            System.out.println("0. Exit");
+        }
+        
+        public static void main(String[] args){
+
+            while (true) {
+                displayMenu();
+                
+                System.out.print("Choose option: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Driver ID: ");
+                        String did = scanner.nextLine();
+
+                        System.out.print("Enter Name: ");
+                        String dname = scanner.nextLine();
+
+                        System.out.print("Enter Age: ");
+                        int dage = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("Enter Phone: ");
+                        String dphone = scanner.nextLine();
+
+                        System.out.print("Enter License Number: ");
+                        String dlicense = scanner.nextLine();
+
+                        System.out.print("Enter Experience Years: ");
+                        int dexpy = scanner.nextInt();
+                        scanner.nextLine();
+
+                        Driver driver = new Driver(did, dname, dage, dphone, dlicense, dexpy);
+                        drivers.add(driver);
+
+                        System.out.println("Driver added successfully.");
+                        break;
+
+                    case 2:
+                        System.out.print("Enter Bus ID: ");
+                        String bid = scanner.nextLine();
+                    
+                        System.out.print("Enter Bus Number: ");
+                        String bnum = scanner.nextLine();
+                    
+                        System.out.print("Enter Capacity: ");
+                        int bcap = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("Enter Route: ");
+                        String broute = scanner.nextLine();
+
+                        System.out.print("Enter Driver ID to assign : ");
+                        String searchDid = scanner.nextLine();
+
+                        Driver assignedDriver = null;
+                        
+                        for (Driver d : drivers) {
+                            if (d.getId().equalsIgnoreCase(searchDid)) {
+                                assignedDriver = d;
+                                break;
+                            }
+                        }
+                    
+                        Bus bus = new Bus(bid, bnum, bcap, broute, assignedDriver);
+                        bS.addBus(bus);
+
+                        break;
+                    
+                    case 3:
+                        System.out.print("Enter Passenger ID: ");
+                        String pid = scanner.nextLine();
+
+                        System.out.print("Enter Name: ");
+                        String pname = scanner.nextLine();
+
+                        System.out.print("Enter Age: ");
+                        int page = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("Enter Phone: ");
+                        String pphone = scanner.nextLine();
+
+                        System.out.print("Enter Email: ");
+                        String pemail = scanner.nextLine();
+
+                        Passenger passenger = new Passenger(pid, pname, page, pphone, pemail);
+                        passengers.add(passenger);
+
+                        System.out.print("Passenger added successfully. ");
+                        break;
+
+                    case 4:
+                        System.out.print("Enter Ticket ID: ");
+                        String tid = scanner.nextLine();
+
+                        //find passenger
+                        System.out.print("Enter Passenger ID: ");
+                        String passengerId = scanner.nextLine();
+
+                        Passenger selectedPassenger = null;
+
+                        for (Passenger p : passengers) {
+                            if (p.getId().equalsIgnoreCase(passengerId)) {
+                                selectedPassenger = p;
+                                break;
+                            }
+                        }
+
+                        if (selectedPassenger == null) {
+                            System.out.println("Passenger not found.");
+                            break;
+                        }
+
+                        //find bus
+                        System.out.print("Enter Bus ID: ");
+                        String busId = scanner.nextLine();
+                        Bus selectedBus = null;
+                    
+                        try {
+                            selectedBus = bS.searchBus(busId);
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                            break;
+                        }
+
+                        // ticket details 
+                        System.out.print("Enter Seat Number: ");
+                        String seat = scanner.nextLine();
+
+                        System.out.print("Enter Travel Date: ");
+                        String date = scanner.nextLine();
+
+                        System.out.print("Enter Fare: ");
+                        double fare = scanner.nextDouble();
+                        scanner.nextLine();
+
+                        Ticket ticket = new Ticket(tid, selectedPassenger, selectedBus, seat, date, fare);
+                        tS.bookTicket(ticket);
+
+                        break;
+
+                    case 5:
+                        bS.displayAllBuses();
+                        break;
+
+                    case 6:
+                        tS.displayAllTickets();
+                        break;
+
+                    case 7:
+                        fM.createFile("tickets.txt");
+                        StringBuilder data = new StringBuilder();
+
+                        for(Ticket t : tS.getTickets()){
+                            data.append("Ticket ID: ").append(t.getTicketId()) 
+                            .append("\nPassenger: ").append(t.getPassenger().getName()) 
+                            .append("\nBus Number: ").append(t.getBus().getBusNumber()) 
+                            .append("\nSeat Number: ").append(t.getSeatNumber()) 
+                            .append("\nTravel Date: ").append(t.getTravelDate()) 
+                            .append("\nFare: ").append(t.getFare()) 
+                            .append("\n---------------------------\n"); 
+                        }
+                        //write the text file
+                        fM.writeTextFile("tickets.txt", data.toString());
+                    
+                        //serializati0n
+                        fM.saveObjects("tickets.dat", tS.getTickets());
+                        break;
+
+                    case 8:
+                        //read the text file
+                        System.out.println("\nContents of tickets.txt\n");
+                        fM.readTextFile("tickets.txt"); 
+                    
+                        //load serialized objects
+                        ArrayList<?> loaded = fM.loadObjects("tickets.dat");
+                        System.out.println("\nLoaded Objects: " + loaded.size());
+                        break;
+
+                    case 0:
+                        scanner.close();
+                        System.out.println("Exiting system...");
+                        return;
+
+                    default:
+                        System.out.println("Invalid choice!");
+                
+            } 
+        }   
+    }     
+}
