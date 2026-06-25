923aae1b330b55845562c2ec8339fcefd32961b1
tsedi4567 <hahu13574@gmail.com>
2026-06-24 22:14:19 -0700

    Update TicketService.java

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -3,6 +3,7 @@ package service;
 import model.Ticket;
 import model.Passenger;
 import model.Bus;
+import exception.TicketNotFoundException;
 
 import java.util.ArrayList;
 
@@ -17,7 +18,7 @@ public class TicketService {
         System.out.println("Ticket booked successfully.");
     }
 
-    // merhod overloading to book a ticket
+    // method overloading to book a ticket
     public void bookTicket(String ticketId, Passenger passenger, Bus bus, String seatNumber, String travelDate, double fare) {
 
         Ticket ticket = new Ticket(ticketId, passenger, bus, seatNumber, travelDate, fare);
@@ -40,33 +41,33 @@ public class TicketService {
     }
 
     // search for a ticket by its ID
-    public Ticket searchTicket(String ticketId) {
+    public Ticket searchTicket(String ticketId) throws TicketNotFoundException {
         for (Ticket ticket : tickets) {
             if (ticket.getTicketId().equalsIgnoreCase(ticketId)) {
                 return ticket;
             }
         }
-        return null; // Return null if ticket not found
+        throw new TicketNotFoundException("Ticket not found with ID: " + ticketId);
     }
 
     // cancel a ticket by its ID
     public void cancelTicket(String ticketId) {
-        Ticket ticket = searchTicket(ticketId);
-
-        if (ticket != null) {
+        try{
+            Ticket ticket = searchTicket(ticketId);
             tickets.remove(ticket);
             System.out.println("Ticket cancelled successfully.");
 
-        }else{
-            System.out.println("Ticket not found.");
+        } catch (TicketNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Cancel ticket operation completed.");
         }  
     }
 
     // update ticket details
     public void updateTicket(String ticketId, String newSeatNumber, String newTravelDate, double newFare) {
-        Ticket ticket = searchTicket(ticketId);
-
-        if (ticket != null) {
+        try{
+            Ticket ticket = searchTicket(ticketId);
         
             ticket.setSeatNumber(newSeatNumber);
             ticket.setTravelDate(newTravelDate);
@@ -74,8 +75,10 @@ public class TicketService {
 
             System.out.println("Ticket updated successfully.");
 
-        } else {
-            System.out.println("Ticket not found.");
+        } catch(TicketNotFoundException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Update ticket operation completed.");
         }
     }
     
@@ -83,4 +86,3 @@ public class TicketService {
         return tickets;
      }
 }
-
