6cc55f77e508d3f4bed5e503f593458b4ea1366e
El-olam <yedidya.amanuel.official@gmail.com>
2026-06-25 12:34:09 +0300

    Update FileManager.java

--- src/file/FileManager.java
+++ src/file/FileManager.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 
 public class FileManager {
     
-    // save objects using serialization
-    public void saveObjects(String fileName, ArrayList<?> list) {
+    // Saves object data using serialization
+public void saveObjects(String fileName, ArrayList<?> list) {
 
         try(ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(fileName))){
 
